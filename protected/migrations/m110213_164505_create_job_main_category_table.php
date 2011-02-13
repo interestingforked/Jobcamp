f0cc5e250f7da91e9f15714212c58850eb1de212
Prashant P Shah <pshah.mumbai@gmail.com>
2011-02-13 22:42:51 +0530

    Updated job main and sub category to include foriegn key
    Signed-off-by: Prashant P Shah <pshah.mumbai@gmail.com>

--- protected/migrations/m110213_164505_create_job_main_category_table.php
+++ protected/migrations/m110213_164505_create_job_main_category_table.php
@@ -4,7 +4,7 @@ class m110213_164505_create_job_main_category_table extends CDbMigration
 {
 	public function up()
 	{
-		$this->createTable('job_main_category'), array(
+		$this->createTable('job_main_category', array(
 			'id' => 'pk',
 			'name' => 'varchar(255) NOT NULL',
 		));
--- protected/migrations/m110213_164535_create_job_sub_category_table.php
+++ protected/migrations/m110213_164535_create_job_sub_category_table.php
@@ -4,10 +4,12 @@ class m110213_164535_create_job_sub_category_table extends CDbMigration
 {
 	public function up()
 	{
-		$this->createTable('job_sub_category'), array(
+		$this->createTable('job_sub_category', array(
 			'id' => 'pk',
+			'job_main_category_id' => 'int(11) NOT NULL',
 			'name' => 'varchar(255) NOT NULL',
 		));
+		$this->addForeignKey('job_main_category_present', 'job_sub_category', 'job_main_category_id', 'job_main_category', 'id', 'RESTRICT', 'RESTRICT');
 	}
 
 	public function down()
